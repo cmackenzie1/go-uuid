f66ac8aedf710da54d0616f316d8fad1119c0684
Cole MacKenzie <colemackenzie1@gmail.com>
2023-01-24 10:13:29 -0800

    add FAQ section to README.md and update example for err handling

--- example/example.go
+++ example/example.go
@@ -7,9 +7,15 @@ import (
 )
 
 func main() {
-	v4, _ := uuid.NewV4()
+	v4, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("UUIDv4: %s\n", v4) // c07526de-40e5-418f-93d1-73ba20d2ac2c
 
 	v7, _ := uuid.NewV7()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("UUIDv7: %s\n", v7) // 0185e1af-a3c1-704f-80f5-6fd2f8387f09
 }
